cc9f4c6a0291454ed354c9bc11c9af280f7dc03e
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-01-29 10:31:13 -0500

    about to make our first custom component

--- hello-react/src/App.tsx
+++ hello-react/src/App.tsx
@@ -2,9 +2,22 @@ import './App.css'
 
 function App() {
 
+  //This is a comment in Typescript
+
   return (
     <>
       <h1>Hello React!</h1>
+
+      <p>We are writing in TSX, which is like HTML for React Typscript apps</p>
+
+      <h3>React will reload the app with every saved change to the source code</h3>
+
+      {/* This is a comment in TSX. autogen one with: ctrl + / */}
+
+      <p>Check out this custom component: </p>
+
+      
+
     </>
   )
 }
